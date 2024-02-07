b994f51666964274b8e08699662e9372de9cba35
Vladimir Jakubik <vladimir.jakubik77@gmail.com>
2024-02-07 10:58:48 +0100

    Basic model - functioning

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ import axios from "axios";
 
 const port = 3000;
 const app = express();
+const API_URL = "https://v2.jokeapi.dev";
+let categories = [];
+
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
-})
+app.get("/", async (req, res) => {
+    try {
+    const result = await axios.get(API_URL + "/joke/Any");
+    const receivedJoke = {
+        joke: result.data.joke,
+        setup: result.data.setup,
+        delivery: result.data.delivery,
+    }; 
+    res.render("index.ejs", receivedJoke);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
